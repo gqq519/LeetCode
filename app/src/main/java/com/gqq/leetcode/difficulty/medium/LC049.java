f5a5f90f09d4a0e5b448228f89ed01b94f3414e4
gqq <gqq519@163.com>
2020-05-17 21:31:17 +0800

    -: add 49

---
+++ app/src/main/java/com/gqq/leetcode/difficulty/medium/LC049.java
@@ -0,0 +1,40 @@
+package com.gqq.leetcode.difficulty.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * Note:
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ */
+public class LC049 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String str = String.valueOf(chars);
+            if (!map.containsKey(str)) {
+                map.put(str, new ArrayList<String>());
+            }
+            map.get(str).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
